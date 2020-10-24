4f8c5f4e3f262e60598d8fef099e4f4a566dedc5
yoshashriyan <yosha.shriyan@gmail.com>
2020-10-24 13:18:15 +0530

    some errors in rankhospital..mostly done

--- rankhospital.R
+++ rankhospital.R
@@ -1,8 +1,5 @@
 rankhospital <- function(state, oc, num = "best"){
-  if(num == "best")
-    num == 1
-  if(num == "worst")
-    num == -1
+
   if(state %in% snames && oc %in% poss_out)
   {
     s1 <- outcome[outcome$State == state, ]
@@ -19,7 +16,7 @@ rankhospital <- function(state, oc, num = "best"){
       cnum = 23;
       srtd <- s1[order(s1$Hospital.30.Day.Death..Mortality..Rates.from.Pneumonia),]
     }
-    View(srtd)
+    #View(srtd)
     n <- nrow(s1)
 
     for(i in 1:n){
@@ -28,10 +25,16 @@ rankhospital <- function(state, oc, num = "best"){
     }
     x <- srtd[-c(i:n)]
     nn <- nrow(x)
-    rank <- 1:nn
-    result <- cbind(x$Hospital.Name,rank)
-    #result <- sort(result)
-    return(result)
+    #rank <- 1:nn
+    myList <- list(x$Hospital.Name,x[, cnum])
+    tmp <- as.data.frame(myList)
+    colnames(tmp) <- c("Hospital", "Rate")
+    tmp <- tmp[order(tmp[, 2], tmp[, 1]), ]
+    if(num == "best")
+      num <- 1
+    if(num == "worst")
+      num <- nn
+    return(tmp[cnum, 1])
   }
   else
     print("Invalid Input");
